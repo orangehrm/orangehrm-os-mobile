457fdf5313bc2008291fce3f05ef6e7860ed87fc
Rajitha Kumara <rajitha@orangehrm.us.com>
2020-07-28 13:09:00 +0530

    OHRM-746:Add tests for api service

---
+++ __tests__/services/api-test.ts
@@ -0,0 +1,97 @@
+/*
+ * This file is part of OrangeHRM
+ *
+ * Copyright (C) 2020 onwards OrangeHRM (https://www.orangehrm.com/)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+import {
+  isAccessTokenExpired,
+  getMessageAlongWithGenericErrors,
+  getMessageAlongWithResponseErrors,
+} from 'services/api';
+import {AuthenticationError} from 'services/errors/authentication';
+
+describe('services/api', () => {
+  test('isAccessTokenExpired::check with null', () => {
+    const result = isAccessTokenExpired(null);
+    expect(result).toBeTruthy();
+  });
+
+  test('isAccessTokenExpired::check with now', () => {
+    const date = new Date();
+    const result = isAccessTokenExpired(date.toISOString());
+    expect(result).toBeTruthy();
+  });
+
+  test('isAccessTokenExpired::check with future time', () => {
+    const date = new Date();
+    date.setMinutes(date.getMinutes() + 5);
+    const result = isAccessTokenExpired(date.toISOString());
+    expect(result).toBeFalsy();
+  });
+
+  test('getMessageAlongWithGenericErrors::invalid error object + default message', () => {
+    let result = getMessageAlongWithGenericErrors({});
+    expect(result).toBe('Unexpected Error Occurred.');
+    result = getMessageAlongWithGenericErrors([], 'Default Error');
+    expect(result).toBe('Default Error');
+  });
+
+  test('getMessageAlongWithGenericErrors::check throw from fetch when offline', () => {
+    const result = getMessageAlongWithGenericErrors(
+      new TypeError('Network request failed'),
+    );
+    expect(result).toBe('Connection Error! Operation Couldn’t Be Completed.');
+  });
+
+  test('getMessageAlongWithGenericErrors::check throw from fetch when offline', () => {
+    const result = getMessageAlongWithGenericErrors(
+      new AuthenticationError('Authentication Failed.'),
+    );
+    expect(result).toBe('Authentication Failed.');
+  });
+
+  test('getMessageAlongWithResponseErrors::api response errors', () => {
+    let result = getMessageAlongWithResponseErrors({
+      error: {status: '404', text: 'No Records Found'},
+    });
+    expect(result).toBe('No Records Found.');
+
+    result = getMessageAlongWithResponseErrors({
+      error: ['Saving Failed'],
+    });
+    expect(result).toBe('Saving Failed');
+
+    expect(() => {
+      getMessageAlongWithResponseErrors({
+        error: ['Employee is terminated'],
+      });
+    }).toThrow('Employee is terminated');
+
+    expect(() => {
+      getMessageAlongWithResponseErrors({
+        error: ['Employee not assigned'],
+      });
+    }).toThrow('Employee not assigned');
+
+    expect(() => {
+      getMessageAlongWithResponseErrors({
+        error: ['Account disabled'],
+      });
+    }).toThrow('Account disabled');
+  });
+});
