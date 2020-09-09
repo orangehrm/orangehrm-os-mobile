23965955fb4a78753447d11bb97c5a3846083c6c
Rajitha Kumara <rajitha@orangehrm.us.com>
2020-09-09 12:23:16 +0530

    OHRM-851:Fix unit test failure because of @react-native-community/netinfo

--- __tests__/setup.js
+++ __tests__/setup.js
@@ -1,3 +1,7 @@
+// https://github.com/react-native-community/react-native-netinfo#errors-while-running-jest-tests
+import mockRNCNetInfo from '@react-native-community/netinfo/jest/netinfo-mock.js';
+jest.mock('@react-native-community/netinfo', () => mockRNCNetInfo);
+
 // https://github.com/react-navigation/react-navigation/issues/1792
 jest.mock('@react-navigation/drawer', () => {
   return {
