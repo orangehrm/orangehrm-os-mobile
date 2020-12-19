9d780ac6a3461b6e6cdd678f6fbdc68b825707ed
Rajitha Kumara <rajitha@orangehrm.us.com>
2020-12-19 10:44:48 +0530

    OHRM-973:Update depandancies

---
+++ __mocks__/@react-native-async-storage/async-storage.js
@@ -0,0 +1,2 @@
+import * as mock from '@react-native-async-storage/async-storage/jest/async-storage-mock';
+export default mock;
--- src/services/storage.ts
+++ src/services/storage.ts
@@ -18,7 +18,7 @@
  *
  */
 
-import AsyncStorage from '@react-native-community/async-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const get = async (key: string): Promise<string | null | undefined> => {
   try {
