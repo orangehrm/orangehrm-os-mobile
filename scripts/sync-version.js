3fd3b63e37bc72b48660aff8234299a891ac2ba0
Rajitha Kumara <rajitha@orangehrm.us.com>
2023-03-08 16:04:38 +0530

    Bump version to 3.0.0-alpha1

--- scripts/sync-version.js
+++ scripts/sync-version.js
@@ -68,7 +68,7 @@ fs.readFile(gradlePropertiesPath, 'utf8', (error, gradleProperties) => {
 const projectpbxprojPath = path.join(
   __dirname,
   '..',
-  'ios/OrangeHRM.xcodeproj/project.pbxproj',
+  'ios/OSMobile.xcodeproj/project.pbxproj',
 );
 
 fs.readFile(projectpbxprojPath, 'utf8', (error, projectpbxproj) => {
