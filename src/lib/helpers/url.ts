c24ef253759820791a240a44c64f0e2f9a831607
ManulMax <manul@orangehrm.us.com>
2023-03-23 11:41:56 +0530

    OHRM5X-1964: Fix test failing

--- src/lib/helpers/url.ts
+++ src/lib/helpers/url.ts
@@ -1,36 +1,34 @@
-export const checkUrl = () =>
-  // url: string,
-  // withPath: boolean = true,
-  // withProtocol: boolean = true,
-  {
-    //need to change
-    return true;
-    // if (withPath && withProtocol) {
-    //   return isValid(
-    //     url.match(
-    //       /^((https):\/\/)([\p{L}\p{N}\p{S}\-.])+(\.?([\p{L}\p{N}]|xn--[\p{L}\p{N}-]+)+\.?)(:[0-9]+)?((\/[\p{L}.]+)+)?$/gu,
-    //     ),
-    //   );
-    // } else if (withProtocol) {
-    //   return isValid(
-    //     url.match(
-    //       /^((https):\/\/)([\p{L}\p{N}\p{S}\-.])+(\.?([\p{L}\p{N}]|xn--[\p{L}\p{N}-]+)+\.?)(:[0-9]+)?$/gu,
-    //     ),
-    //   );
-    // } else if (withPath) {
-    //   return isValid(
-    //     url.match(
-    //       /^([\p{L}\p{N}\p{S}\-.])+(\.?([\p{L}\p{N}]|xn--[\p{L}\p{N}-]+)+\.?)(:[0-9]+)?(\/[\p{L}.]+)+?$/gu,
-    //     ),
-    //   );
-    // } else {
-    //   return isValid(
-    //     url.match(
-    //       /^([\p{L}\p{N}\p{S}\-.])+(\.?([\p{L}\p{N}]|xn--[\p{L}\p{N}-]+)+\.?)(:[0-9]+)?$/gu,
-    //     ),
-    //   );
-    // }
-  };
+export const checkUrl = (
+  url: string,
+  withPath: boolean = true,
+  withProtocol: boolean = true,
+) => {
+  if (withPath && withProtocol) {
+    return isValid(
+      url.match(
+        /^((https):\/\/)([\p{L}\p{N}\p{S}\-.])+(\.?([\p{L}\p{N}]|xn--[\p{L}\p{N}-]+)+\.?)(:[0-9]+)?((\/[\p{L}.]+)+)?$/gu,
+      ),
+    );
+  } else if (withProtocol) {
+    return isValid(
+      url.match(
+        /^((https):\/\/)([\p{L}\p{N}\p{S}\-.])+(\.?([\p{L}\p{N}]|xn--[\p{L}\p{N}-]+)+\.?)(:[0-9]+)?$/gu,
+      ),
+    );
+  } else if (withPath) {
+    return isValid(
+      url.match(
+        /^([\p{L}\p{N}\p{S}\-.])+(\.?([\p{L}\p{N}]|xn--[\p{L}\p{N}-]+)+\.?)(:[0-9]+)?(\/[\p{L}.]+)+?$/gu,
+      ),
+    );
+  } else {
+    return isValid(
+      url.match(
+        /^([\p{L}\p{N}\p{S}\-.])+(\.?([\p{L}\p{N}]|xn--[\p{L}\p{N}-]+)+\.?)(:[0-9]+)?$/gu,
+      ),
+    );
+  }
+};
 
 export const checkDomain = (url: string) => {
   return isValid(
