dec6e3a8a77b9a90310c17d677d93c9a2ff1a5f0
Shehan <shehan@orangeHRM.us.com>
2026-05-21 07:32:08 +0530

    Fixed the issue based on the PR feedback.

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
  */
 
 import 'react-native-gesture-handler';
-import 'react-native-reanimated';
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
